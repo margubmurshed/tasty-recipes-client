c833c4767b78fb1db53335c1fe2054abdf429258
Margub Murshed <murshedmargub@gmail.com>
2023-05-04 23:27:29 +0600

    show toast on favorite button click added

--- src/pages/ChefRecipes/ChefRecipes.jsx
+++ src/pages/ChefRecipes/ChefRecipes.jsx
@@ -6,12 +6,14 @@ import RecipeCard from "./RecipeCard/RecipeCard";
 const ChefRecipes = () => {
   const [chefDetails, chefRecipes] = useLoaderData();
   const [favorites, setFavorites] = useState([]);
+  const [message, setMessage] = useState("")
   const { id: chefID } = chefDetails;
 
   const handleSetFavorite = (id) => {
     const updatedFavorites = [...favorites, { cID: chefID, recipeID: id }];
     setFavorites(updatedFavorites);
     localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    setMessage("The recipe is your favourite!")
   };
 
   useEffect(() => {
@@ -22,7 +24,7 @@ const ChefRecipes = () => {
   return (
     <div>
       <ChefDetails chefDetails={chefDetails} />
-      <div className="container mx-auto p-5 py-16">
+      <div className="container mx-auto p-5 py-16 relative">
         <h2 className="text-3xl text-center font-bold">Chef's Recipes</h2>
         <div className="grid md:grid-cols-3 gap-3 mt-5">
           {chefRecipes.map((recipe) => {
@@ -39,6 +41,15 @@ const ChefRecipes = () => {
             );
           })}
         </div>
+        {message && (
+        <div className="toast toast-top toast-end absolute top-0 right-0" onClick={() => setMessage("")}>
+          <div className="alert alert-success">
+            <div>
+              <span>{message}</span>
+            </div>
+          </div>
+        </div>
+      )}
       </div>
     </div>
   );
