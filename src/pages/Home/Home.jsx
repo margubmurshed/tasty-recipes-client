6df4900bce242f52a1c784ac661d58ec5d2a2774
Margub Murshed <murshedmargub@gmail.com>
2023-05-05 16:15:58 +0600

    added loader at home chefs section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,17 +5,25 @@ import Slider from "./Slider/Slider";
 import { useState } from "react";
 const Home = () => {
   const [chefs, setChefs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://tasty-recipes-server-margubtech-gmailcom.vercel.app/allChefsDetails")
     .then(data => data.json())
-    .then(data => setChefs(data.chefs))
+    .then(data => {
+      setChefs(data.chefs);
+      setLoading(false)
+    })
+    .catch(err => {
+      console.log(err);
+      setLoading(false)
+    })
   }, [])
 
   return (
     <div>
       <Hero />
-      <Chefs chefs={chefs}/>
+      <Chefs chefs={chefs} loading={loading}/>
       <Slider />
     </div>
   );
